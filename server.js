2d3c81d0fbed6976b131a11000c51129ab8f279e
djrloria <djrloria@gmail.com>
2017-06-04 20:18:59 -0500

    heroku test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var people = require('./app/data/people')
 
 //Express app
 var app = express();
-var PORT = 3000;
+var PORT = (process.env.PORT || 3000);
 
 //Middleware bodyparser setup
 app.use(bodyParser.json());
